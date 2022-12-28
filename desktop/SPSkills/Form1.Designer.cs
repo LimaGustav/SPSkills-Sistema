eac4a465921bc4cad26b69c9e325c56b40b833d1
LimaGustav <lima.gustavo100@hotmail.com>
2022-12-28 17:15:19 -0300

    Desktop Base

--- desktop/SPSkills/Form1.Designer.cs
+++ desktop/SPSkills/Form1.Designer.cs
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.customPanel1 = new SPSkills.CustomControls.CustomPanel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.customPanel1);
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            // 
+            // customPanel1
+            // 
+            this.customPanel1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.customPanel1.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.customPanel1.BorderColor = System.Drawing.Color.Transparent;
+            this.customPanel1.BorderRadius = 20;
+            this.customPanel1.BorderSize = 0;
+            this.customPanel1.ForeColor = System.Drawing.Color.White;
+            this.customPanel1.Location = new System.Drawing.Point(336, 163);
+            this.customPanel1.Name = "customPanel1";
+            this.customPanel1.Size = new System.Drawing.Size(150, 40);
+            this.customPanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private CustomControls.CustomPanel customPanel1;
     }
 }
 
--- desktop/SPSkills/Form1.cs
+++ desktop/SPSkills/Form1.cs
@@ -16,5 +16,10 @@ namespace SPSkills
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
